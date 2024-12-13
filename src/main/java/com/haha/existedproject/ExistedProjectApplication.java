d1f86d9bada28916d14ebd692316ee1d81e1981a
huangjiali-git <2401754616@qq.com>
2024-12-14 01:55:45 +0800

    1111

---
+++ src/main/java/com/haha/existedproject/ExistedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.haha.existedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExistedProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExistedProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haha/existedproject/ExistedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haha.existedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExistedProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
